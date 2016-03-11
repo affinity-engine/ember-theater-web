2928ad5130c731f503c703523e1107432ae94c63
null null null <null@ember.theater>
2016-03-10 18:49:16 -0800

    move to eslint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,8 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var path = require('path');
+var jsStringEscape = require('js-string-escape');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -8,21 +10,36 @@ module.exports = function(defaults) {
     babel: {
       includePolyfill: true,
       stage: 1
-    }
+    },
+
+    'ember-cli-qunit': {
+      useLintTree: false
+    },
+
+    // eslint: {
+    //   testGenerator: eslintTestGenerator
+    // }
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  /* eslintTestGenerator */
+  function render(errors) {
+    if (!errors) { return ''; }
+    return errors.map(function(error) {
+      return error.line + ':' + error.column + ' ' +
+        ' - ' + error.message + ' (' + error.ruleId +')';
+    }).join('\n');
+  }
+
+  // Qunit test generator
+  function eslintTestGenerator(relativePath, errors) {
+    var pass = !errors || errors.length === 0;
+    return "import { module, test } from 'qunit';\n" +
+      "module('ESLint - " + path.dirname(relativePath) + "');\n" +
+      "test('" + relativePath + " should pass ESLint', function(assert) {\n" +
+      "  assert.ok(" + pass + ", '" + relativePath + " should pass ESLint." +
+      jsStringEscape("\n" + render(errors)) + "');\n" +
+      "});\n";
+  }
 
   app.import('bower_components/showdown/dist/showdown.min.js');
 
