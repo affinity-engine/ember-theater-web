57e1467ef52f9efb6dc1a01ac86fa394235962a7
null null null <null@ember.theater>
2016-03-17 09:18:59 -0700

    update theater

--- app/ember-theater/director/scenes/director/character.js
+++ app/ember-theater/director/scenes/director/character.js
@@ -10,63 +10,63 @@ export default Scene.extend({
 
     const bitsy = this.Character('bitsy').initialExpression('bitsy-laughing').position('center', 0).transition('transition.bounceUpIn', 1000).position('centerRight', 500).namePosition('right');
     emma.position('centerLeft', 500).Expression('emma-panic');
-    await bitsy.Text('theaters.character.intro.bitsyGreeting');
-
-    bitsy.Expression('bitsy-happy');
-    await emma.Expression('emma-embarrassed').Text('theaters.character.intro.emmaHiding');
-    emma.Expression('emma-neutral');
-    await bitsy.Text('theaters.character.intro.bitsyHiding');
-
-    emma.Expression('emma-laughing');
-    await emma.Text('theaters.character.intro.emmaCharacters');
-    emma.Expression('emma-neutral').delay(250);
-    bitsy.Expression('bitsy-surprised');
-    await bitsy.Text('theaters.character.intro.bitsyCharacters');
-
-    bitsy.Expression('bitsy-neutral');
-    await emma.Text('theaters.character.intro.emmaPosition');
-    await emma.position('left').Text('theaters.character.intro.emmaPositionLeft');
-    await emma.position('farLeft').Text('theaters.character.intro.emmaPositionFarLeft');
-    bitsy.Expression('bitsy-bored');
-    await emma.position('offLeft').Text('theaters.character.intro.emmaPositionOffLeft');
-    await emma.position('centerLeft').Text('theaters.character.intro.emmaPositionCenterLeft');
-    await emma.position('nudgeLeft').Text('theaters.character.intro.emmaPositionNudgeLeft');
-    await emma.position('nudgeLeft').Text('theaters.character.intro.emmaPositionNudgeLeftAgain');
-    await emma.position('nudgeUp').Text('theaters.character.intro.emmaPositionNudgeUp');
-    await emma.position('nudgeDown').Text('theaters.character.intro.emmaPositionNudgeDown');
-    await emma.position('nudgeBack').Text('theaters.character.intro.emmaPositionNudgeBack');
-    await emma.position('nudgeForward').Text('theaters.character.intro.emmaPositionNudgeForward');
-    await emma.Text('theaters.character.intro.emmaPositionConfig');
-
-    await bitsy.Text('theaters.character.intro.bitsyPosition');
-    await emma.Text('theaters.character.intro.emmaPosition2');
-    bitsy.position('farRight', 2000).Expression('bitsy-neutral');
-    await bitsy.Text('theaters.character.intro.bitsyPosition2');
-    emma.position('farRight', 3000).position('right', 1000).Expression('emma-laughing');
-    bitsy.Expression('bitsy-laughing').delay(3000);
-    await emma.Text('theaters.character.intro.emmaPosition3');
-    await bitsy.Text('theaters.character.intro.bitsyPosition3');
-    await this.Pause(250);
-
-    emma.position('centerLeft', 2500).Expression('emma-neutral');
-    await bitsy.position('centerRight', 2500).Expression('bitsy-neutral').delay(300);
-    await emma.Text('theaters.character.intro.emmaTransition');
-    emma.transition('callout.bounce', 750).transition('callout.bounce', 600).transition('callout.bounce').transition('transition.perspectiveDownOut', 1000).transition('transition.bounceUpIn', 1000).delay(250);
-    await bitsy.Expression('bitsy-laughing').delay(1750).Text('theaters.character.intro.bitsyTransition').delay(2000);
-
-    await emma.Text('theaters.character.intro.emmaTransition2');
-    bitsy.transition('callout.bounce').Expression('bitsy-neutral');
-    await bitsy.Text('theaters.character.intro.bitsyTransition2');
-    await emma.Text('theaters.character.intro.emmaTransition3');
-    bitsy.transition('callout.swing');
-    await bitsy.Text('theaters.character.intro.bitsyTransition3');
-    emma.Expression('emma-laughing');
-    await emma.Text('theaters.character.intro.emmaTransition4');
-    bitsy.transition('transition.bounceUpIn').Expression('bitsy-laughing');
-    await bitsy.Text('theaters.character.intro.bitsyTransition4');
-    await emma.Text('theaters.character.intro.emmaTransition5');
-    bitsy.transition('callout.bounce').transition('callout.swing').transition('transition.bounceUpIn').Expression('bitsy-laughing');
-    await bitsy.Text('theaters.character.intro.bitsyTransition5');
+    // await bitsy.Text('theaters.character.intro.bitsyGreeting');
+    //
+    // bitsy.Expression('bitsy-happy');
+    // await emma.Expression('emma-embarrassed').Text('theaters.character.intro.emmaHiding');
+    // emma.Expression('emma-neutral');
+    // await bitsy.Text('theaters.character.intro.bitsyHiding');
+    //
+    // emma.Expression('emma-laughing');
+    // await emma.Text('theaters.character.intro.emmaCharacters');
+    // emma.Expression('emma-neutral').delay(250);
+    // bitsy.Expression('bitsy-surprised');
+    // await bitsy.Text('theaters.character.intro.bitsyCharacters');
+    //
+    // bitsy.Expression('bitsy-neutral');
+    // await emma.Text('theaters.character.intro.emmaPosition');
+    // await emma.position('left').Text('theaters.character.intro.emmaPositionLeft');
+    // await emma.position('farLeft').Text('theaters.character.intro.emmaPositionFarLeft');
+    // bitsy.Expression('bitsy-bored');
+    // await emma.position('offLeft').Text('theaters.character.intro.emmaPositionOffLeft');
+    // await emma.position('centerLeft').Text('theaters.character.intro.emmaPositionCenterLeft');
+    // await emma.position('nudgeLeft').Text('theaters.character.intro.emmaPositionNudgeLeft');
+    // await emma.position('nudgeLeft').Text('theaters.character.intro.emmaPositionNudgeLeftAgain');
+    // await emma.position('nudgeUp').Text('theaters.character.intro.emmaPositionNudgeUp');
+    // await emma.position('nudgeDown').Text('theaters.character.intro.emmaPositionNudgeDown');
+    // await emma.position('nudgeBack').Text('theaters.character.intro.emmaPositionNudgeBack');
+    // await emma.position('nudgeForward').Text('theaters.character.intro.emmaPositionNudgeForward');
+    // await emma.Text('theaters.character.intro.emmaPositionConfig');
+    //
+    // await bitsy.Text('theaters.character.intro.bitsyPosition');
+    // await emma.Text('theaters.character.intro.emmaPosition2');
+    // bitsy.position('farRight', 2000).Expression('bitsy-neutral');
+    // await bitsy.Text('theaters.character.intro.bitsyPosition2');
+    // emma.position('farRight', 3000).position('right', 1000).Expression('emma-laughing');
+    // bitsy.Expression('bitsy-laughing').delay(3000);
+    // await emma.Text('theaters.character.intro.emmaPosition3');
+    // await bitsy.Text('theaters.character.intro.bitsyPosition3');
+    // await this.Pause(250);
+    //
+    // emma.position('centerLeft', 2500).Expression('emma-neutral');
+    // await bitsy.position('centerRight', 2500).Expression('bitsy-neutral').delay(300);
+    // await emma.Text('theaters.character.intro.emmaTransition');
+    // emma.transition('callout.bounce', 750).transition('callout.bounce', 600).transition('callout.bounce').transition('transition.perspectiveDownOut', 1000).transition('transition.bounceUpIn', 1000).delay(250);
+    // await bitsy.Expression('bitsy-laughing').delay(1750).Text('theaters.character.intro.bitsyTransition').delay(2000);
+    //
+    // await emma.Text('theaters.character.intro.emmaTransition2');
+    // bitsy.transition('callout.bounce').Expression('bitsy-neutral');
+    // await bitsy.Text('theaters.character.intro.bitsyTransition2');
+    // await emma.Text('theaters.character.intro.emmaTransition3');
+    // bitsy.transition('callout.swing');
+    // await bitsy.Text('theaters.character.intro.bitsyTransition3');
+    // emma.Expression('emma-laughing');
+    // await emma.Text('theaters.character.intro.emmaTransition4');
+    // bitsy.transition('transition.bounceUpIn').Expression('bitsy-laughing');
+    // await bitsy.Text('theaters.character.intro.bitsyTransition4');
+    // await emma.Text('theaters.character.intro.emmaTransition5');
+    // bitsy.transition('callout.bounce').transition('callout.swing').transition('transition.bounceUpIn').Expression('bitsy-laughing');
+    // await bitsy.Text('theaters.character.intro.bitsyTransition5');
 
     emma.Expression('emma-neutral').delay(500);
     await emma.Text('theaters.character.intro.emmaInstance');
