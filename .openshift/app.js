e3092cab63885d43955e99c206480b0282988075
null null null <null@ember.theater>
2016-03-19 09:21:59 -0700

    gzip and cache

--- .openshift/app.js
+++ .openshift/app.js
@@ -1,6 +1,8 @@
 const koa = require('koa'),
-      compress = require('koa-compress'),
+      compressible = require('compressible'),
+      fs = require('fs'),
       route = require('koa-route'),
+      mime = require('mime'),
       mount = require('koa-mount'),
       koaStatic = require('koa-static'),
       sysInfo = require('./utils/sys-info'),
@@ -10,18 +12,32 @@ const app = koa();
 
 app.use(route.get('/health', health));
 app.use(route.get('/info/(.*)', info));
+
+app.use(function *(next) {
+  yield next;
+
+  if (this.isIndex) {
+    this.set('Cache-Control', 'public, no-cache, no-store, max-age=0');
+  } else {
+    fs.stat(`./dist${this.url}`, (err, stats) => {
+      this.set('Cache-Control', 'public, max-age=86400');
+      this.set('ETag', stats.mtime);
+    });
+  }
+});
+
 app.use(mount('/', koaStatic('dist', { defer: true })));
 
 app.use(function *(next) {
   if (this.url.indexOf('.') === -1) {
     this.url = '/index.html';
-    this.set('Cache-Control', 'no-cache, no-store');
+    this.isIndex = true;
   }
+
+  this.compress = compressible(mime.lookup(this.url));
 
   yield next;
 });
-
-app.use(compress());
 
 function *health() {
   this.status = 200;
--- start.js
+++ start.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const cluster = require('cluster'),
+      fs = require('fs'),
+      walk = require('walk'),
+      zlib = require('zlib'),
       stopSignals = [
         'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
@@ -8,6 +11,37 @@ const cluster = require('cluster'),
       production = process.env.NODE_ENV == 'production';
 
 let stopping = false;
+
+const walker = walk.walk('./dist');
+
+walker.on("file", function (root, fileStats, next) {
+  fs.readFile(`${root}/${fileStats.name}`, function (err, data) {
+    if (err) return console.log(`Failed to find ${root}/${fileStats.name}`);
+    if (fileStats.name.indexOf('.gz') >= 0) return;
+
+    zlib.gzip(data, function(err, gzippedFile) {
+      if (err) return console.log(`Failed to gzip ${fileStats.name}`);
+
+      fs.writeFile(`${root}/${fileStats.name}.gz`, gzippedFile, function(err) {
+        if (err) {
+          console.log(`Failed to write ${root}/${fileStats.name}.gz`);
+        } else {
+          console.log(`Successfully wrote ${root}/${fileStats.name}.gz`);
+        }
+      });
+    });
+
+    next();
+  });
+});
+
+walker.on("errors", function (root, nodeStatsArray, next) {
+  next();
+});
+
+walker.on("end", function () {
+  console.log('completed gzip');
+});
 
 cluster.on('disconnect', function(worker) {
   if (production) {
