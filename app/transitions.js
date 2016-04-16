135c103631d6465fa411c8fcc52d6e3242cc2d36
null null null <null@ember.theater>
2016-04-16 12:45:53 -0700

    add main route transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,6 +1,12 @@
 export default function() {
+
   this.transition(
     this.toRoute((routeName) => routeName.indexOf('learn') === 0 ),
     this.use('toRight')
+  );
+
+  this.transition(
+    this.toRoute((routeName) => (routeName.match(/\./g) || []).length === 0 || routeName === 'index'),
+    this.use('toDown')
   );
 }
